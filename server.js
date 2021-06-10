b3ac1de0cfcbed65a74a39bf131abb05f0c15a36
ferhatosanoglu <ferhatosanogluo@gmail.com>
2021-06-11 01:37:33 +0300

    refactor : some problems revised

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 4200;
 
-app.use(express.static('./dist/dietitian-app'));
+app.use(express.static(__dirname + "/dist/dietitian-app"));
 
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/dietitian-app' }
-    );
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/dist/dietitian-app/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
+app.listen(PORT, () => {
+    console.log("Ready on http://localhost:" + PORT);
+});
